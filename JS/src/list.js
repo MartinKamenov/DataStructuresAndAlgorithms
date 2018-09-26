2644080c40229bf7e471e13505996882b8d16a79
Martin Kamenov <martinkamenov10@gmail.com>
2018-09-26 22:24:42 +0300

    Added some of the methods for list in JS

--- JS/src/list.js
+++ JS/src/list.js
@@ -1,7 +1,38 @@
 class List {
     constructor() {
-        this.dataSource = [];
-        this.size = 0;
+        this.buffer = [];
+        this._size = 0;
+    }
+
+    add(element) {
+        return this.buffer.push(element);
+    }
+
+    insert(index, element) {
+        if (index < 0 || index > this.buffer.length) {
+            return;
+        }
+
+        this.dataStore.splice(index + 1, 0, element);
+        _size++;
+    }
+
+    contains(element) {
+        const hasBeenFound = this.buffer.includes(function(el) {
+            return el === element;
+        });
+
+        return hasBeenFound;
+    }
+    find(element) {
+        const foundIndex = this.buffer.findIndex(function(el) {
+            return el === element;
+        });
+
+        return foundIndex;
+    }
+    length() {
+        return this._size;
     }
 }
 
