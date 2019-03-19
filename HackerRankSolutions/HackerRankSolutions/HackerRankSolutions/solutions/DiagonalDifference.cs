540b174a075077f13c50b7dc455e9fd940a82e90
MartinKamenov <martinkamenov10@gmail.com>
2019-03-19 19:24:03 +0200

    Added solution for Diagonal Difference

---
+++ HackerRankSolutions/HackerRankSolutions/HackerRankSolutions/solutions/DiagonalDifference.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolutions.solutions
+{
+    public class DiagonalDifference
+    {
+        public static void Solve()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[][] arr = new int[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            }
+
+            int firstDSum = 0;
+            int secondDSum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                firstDSum += arr[i][i];
+                secondDSum += arr[i][n - i - 1];
+            }
+
+            Console.WriteLine(Math.Abs(firstDSum - secondDSum));
+        }
+    }
+}
