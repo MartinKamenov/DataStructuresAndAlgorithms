b5d5be1cc040fabc1d2196615d48404e90373d86
MartinKamenov <martinkamenov10@gmail.com>
2019-09-04 20:00:30 +0300

    Add minMaxSum solution

---
+++ HackerRankSolutions/HackerRankSolutions/HackerRankSolutions/solutions/MinMaxSum.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolutions.solutions
+{
+    public class MinMaxSum
+    {
+        public static void Solve()
+        {
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+
+            Array.Sort(arr);
+
+            long minSum = 0;
+            long maxSum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i < 4)
+                {
+                    minSum += arr[i];
+                }
+
+                if (i != 0)
+                {
+                    maxSum += arr[i];
+                }
+            }
+
+            Console.WriteLine(minSum + " " + maxSum);
+        }
+    }
+}
