9126fed18fc899f5d432c2ed878bdbf6ce9eaf52
MartinKamenov <martinkamenov10@gmail.com>
2019-09-19 20:48:00 +0300

    Add simple array sum solution

---
+++ HackerRankSolutions/HackerRankSolutions/HackerRankSolutions/solutions/SimpleArraySum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolutions.solutions
+{
+    public class SimpleArraySum
+    {
+        public static int Solve(int[] ar)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < ar.Length; i++)
+            {
+                sum += ar[i];
+            }
+
+            return sum;
+        }
+    }
+}
