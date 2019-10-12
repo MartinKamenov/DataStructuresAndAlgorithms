98d58e020861fab17d4c047d968aede49d962dea
MartinKamenov <martinkamenov10@gmail.com>
2019-10-12 12:12:22 +0300

    Add DivisibleSumPairs solution

---
+++ HackerRankSolutions/HackerRankSolutions/HackerRankSolutions/solutions/DivisibleSumPairs.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolutions.solutions
+{
+    public class DivisibleSumPairs
+    {
+        public static int Solve()
+        {
+            int[] ar = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int k = int.Parse(Console.ReadLine());
+
+            Array.Sort(ar);
+
+            int devizible = 0;
+
+            for (int i = 0; i < ar.Length - 1; i++)
+            {
+                for (int j = i + 1; j < ar.Length; j++)
+                {
+                    if ((ar[i] + ar[j]) % k == 0)
+                    {
+                        devizible++;
+                    }
+                }
+            }
+
+            return devizible;
+        }
+    }
+}
