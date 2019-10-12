f5419a07e8759cd59c95903c14f2044ed1ebb320
MartinKamenov <martinkamenov10@gmail.com>
2019-10-12 13:40:10 +0300

    Add migratorybird solution

---
+++ HackerRankSolutions/HackerRankSolutions/HackerRankSolutions/solutions/MigratoryBirds.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolutions.solutions
+{
+    public class MigratoryBirds
+    {
+        public static int Solve()
+        {
+            List<int> arr = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            int[] memo = new int[20000];
+
+            for (int i = 0; i < arr.Count; i++)
+            {
+                memo[arr[i]]++;
+            }
+
+            int maxNumber = 0;
+            int index = 0;
+
+            for (int i = 0; i < memo.Length; i++)
+            {
+                if (memo[i] == 0)
+                {
+                    continue;
+                }
+
+                if (maxNumber < memo[i])
+                {
+                    maxNumber = memo[i];
+                    index = i;
+                }
+            }
+
+            return index;
+        }
+    }
+}
